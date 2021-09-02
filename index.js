0563ba4399a2aca86bc7421f02f25f3e82626b3c
Div <dddivya7980@gmail.com>
2021-09-02 15:48:58 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public/'))
+
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+/
+app.post('/', (req, res)=>{
+    console.log(req.body);
+    res.send('done');
+})
+
+
+app.listen(process.env.PORT || 3000 , function () {
+    console.log('listening on port 3000');
+})
